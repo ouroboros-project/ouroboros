fc2cbc0c7a11c4f56d452ef2127e0e8c2b3289ea
Fernando Aluani <fernando.aluani@fipecafi.org>
2013-02-22 11:53:57 -0300

    Further updates to CMakeLists to modularize languages; added Lua module

--- cmake/ouroboros.cmake
+++ cmake/ouroboros.cmake
@@ -3,6 +3,12 @@ macro (ouroboros_setup_package package_name)
   include (cmake/${package_name}.cmake)
 endmacro (ouroboros_setup_package package_name)
 
+macro (ouroboros_add_langmodule name lib)
+    list(APPEND OUROBOROS_LANGUAGES_LIST name)
+    list(APPEND OUROBOROS_LIBRARIES lib)
+endmacro (ouroboros_add_langmodule name lib)
+
 set (OUROBOROS_LIBRARIES       "")
 set (OUROBOROS_LIBRARIES_DEBUG "")
+set (OUROBOROS_LANGUAGES_LIST "")
 
