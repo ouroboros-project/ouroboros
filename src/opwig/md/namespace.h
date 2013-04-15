df573b2191e58a64446984e8d7bdb4dec74b3b7e
Rewasvat <fernando_aluani@hotmail.com>
2013-04-15 20:31:30 -0300

    [opwig] updated md::Namespace to new md::Scope interface

--- src/opwig/md/namespace.h
+++ src/opwig/md/namespace.h
@@ -4,16 +4,15 @@
 
 #include <opwig/md/scope.h>
 #include <opwig/md/ptr.h>
-#include <opwig/md/variable.h>
-#include <opwig/md/class.h>
-
-#include <string>
-#include <map>
+#include <opwig/md/accessspecifier.h>
 
 namespace opwig {
 namespace md {
 
 // Forward declarations.
+class Namespace;
+class Variable;
+class Class;
 class Function;
 
 /// Metadata class for C++ namespaces.
@@ -25,62 +24,29 @@ class Namespace final : public Scope {
     /// constructor.
     static Ptr<Namespace> Create ();
 
-    /*** NAMESPACE METHODS ***/
+    /// @see opwig::md::Scope::Namespaces
+    virtual Container<Namespace>& Namespaces() override { return namespaces_; }
     
-    /// @see opwig::md::Scope::NestedNamespacesNum
-    size_t NestedNamespacesNum () const override;
+    /// @see opwig::md::Scope::Variables
+    virtual Container<Variable>& Variables() override { return variables_; }
 
-    /// @see opwig::md::Scope::AddNestedNamespace
-    bool AddNestedNamespace (const std::string& name, Ptr<Namespace> nested) override;
-
-    /// @see opwig::md::Scope::NestedNamespace
-    Ptr<const Namespace> NestedNamespace (const std::string& name) const override;
-
-    /// @see opwig::md::Scope::NestedNamespace
-    Ptr<Namespace> NestedNamespace (const std::string& name) override;
-
-    /*** VARIABLE METHODS ***/
+    /// @see opwig::md::Scope::Classes
+    virtual Container<Class>& Classes() override { return classes_; }
     
-    /// @see opwig::md::Scope::AddGlobalVariable
-    bool AddGlobalVariable ( Ptr<Variable> variable) override;
-
-    /// @see opwig::md::Scope::GlobalVariable
-    Ptr<const Variable> GlobalVariable (const std::string& name) const override;
+    /// @see opwig::md::Scope::Functions
+    virtual Container<Function>& Functions() override { return functions_; }
     
-    /// @see opwig::md::Scope::GlobalVariable
-    Ptr<Variable> GlobalVariable (const std::string& name) override;
-    
-    /*** CLASS METHODS ***/
-    
-    /// @see opwig::md::Scope::NestedClassesNum
-    size_t NestedClassesNum () const override;
-
-    /// @see opwig::md::Scope::AddNestedClass
-    bool AddNestedClass (const std::string& name, Ptr<Class> nested) override;
-
-    /// @see opwig::md::Scope::NestedClass
-    Ptr<const Class> NestedClass (const std::string& name) const override;
-    
-    /// @see opwig::md::Scope::NestedClass
-    Ptr<Class> NestedClass (const std::string& name) override;
-
-    /// @see opwig::md::Scope::NestedFunctionsNum
-    size_t NestedFunctionsNum () const;
-
-    /// @see opwig::md::Scope::AddNestedFunction
-    bool AddNestedFunction (Ptr<Function> nested);
-
-    /// @see opwig::md::Scope::NestedFunction
-    Ptr<const Function> NestedFunction (const std::string& name) const;
+    /// @see opwig::md::Scope::SetAccessSpecifier
+    virtual void SetAccessSpecifier(AccessSpecifier access) override;
 
   private:
 
-    std::map<std::string, Ptr<Namespace>> nested_namespaces_;
-    std::map<std::string, Ptr<Class>>     nested_classes_;
-    std::map<std::string, Ptr<Variable>>  global_variables_;
-    std::map<std::string, Ptr<Function>>  nested_functions_;
+    Container<Namespace> namespaces_;
+    Container<Class>     classes_;
+    Container<Variable>  variables_;
+    Container<Function>  functions_;
 
-    Namespace () {}
+    Namespace () { SetAccessSpecifier(AccessSpecifier::PUBLIC); }
 
 };
 
@@ -88,16 +54,11 @@ inline Ptr<Namespace> Namespace::Create () {
     return Ptr<Namespace>(new Namespace);
 }
 
-inline size_t Namespace::NestedNamespacesNum () const {
-    return nested_namespaces_.size();
-}
-
-inline size_t Namespace::NestedClassesNum () const {
-    return nested_classes_.size();
-}
-
-inline size_t Namespace::NestedFunctionsNum() const {
-    return nested_functions_.size();
+inline void SetAccessSpecifier(AccessSpecifier access) {
+    namespaces_.SetAccessSpecifier(access);
+    classes_.SetAccessSpecifier(access);
+    variables_.SetAccessSpecifier(access);
+    functions_.SetAccessSpecifier(access);
 }
 
 } // namespace md
