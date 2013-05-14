10bbec09272f5945aecc6e91fa4af95487c58761
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-05-13 21:26:20 -0300

    [opwig] Some space adjustments.

--- src/opwig/gen/proxygenerator.h
+++ src/opwig/gen/proxygenerator.h
@@ -9,16 +9,17 @@
 namespace opwig {
 
 namespace md {
-
 class Scope;
-
 }
 
 namespace gen {
 
 class ProxyGenerator final {
+
   public:
+
     ProxyGenerator (const std::string& the_output_dir) {}
+
     /// Generates proxies from the classes in the given scope.
     //  @param the_scope Scope whose classes will be proxied.
     //  @return The number of proxied classes.
