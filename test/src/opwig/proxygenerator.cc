a8aacb098927cb70dc4fcac0f05e4f5e4c4ad4ab
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-05-13 22:58:02 -0300

    [opwig] Proxy generator test starts to consider generated code.

--- test/src/opwig/proxygenerator.cc
+++ test/src/opwig/proxygenerator.cc
@@ -27,8 +27,10 @@ class ProxyGeneratorTest : public ::testing::Test {
         given_scope_->AddNestedClass(class_name, Class::Create(class_name, {}));
     }
 
-    void AddVirtualClass() {
-
+    void AddVirtualClass(const string& class_name) {
+        Ptr<Class> the_class = Class::Create(class_name, {});
+        given_scope_->AddNestedClass(class_name, the_class);
+        the_class->AddNestedFunction(Function::Create("~"+class_name, "", {}, false));
     }
 
     bool Open(const string& filepath) {
@@ -38,13 +40,34 @@ class ProxyGeneratorTest : public ::testing::Test {
         return generated_code_.good();
     }
 
+    bool GenerateCodeMatches(const string& source_file, const string& expected_code) {
+        if (!Open(OUROBOROS_TEST_DUMP_DIR "/" + source_file))
+            return false;
+        return false;
+    }
+
 };
 
 TEST_F(ProxyGeneratorTest, NoClassesAtAll) {
-    EXPECT_EQ(0u, Generate());
+    EXPECT_EQ(0u, Generate()) << "There is no class.";
 }
 
 TEST_F(ProxyGeneratorTest, SingleNonVirtualClass) {
-    EXPECT_EQ(0u, Generate());
+    AddNonVirtualClass();
+    EXPECT_EQ(0u, Generate()) << "That class does not have a virtual destructor.";
+}
+
+TEST_F(ProxyGeneratorTest, SingleVirtualClass) {
+    AddVirtualClass("VirtualClass");
+    EXPECT_EQ(1u, Generate()) << "Did not found class with virtual destructor, or found too many";
+    string expected_code =
+        "#ifndef OUROBOROS_GENERATED_VirtualClass_H_\n"
+        "#define OUROBOROS_GENERATED_VirtualClass_H_\n"
+        "namespace generated {\n"
+        "class VirtualClass_Proxy : public VirtualClass {};\n"
+        "} // namespace generated\n"
+        "#endif\n";
+    EXPECT_TRUE(GenerateCodeMatches("virtualclass_proxy.h", expected_code))
+        << "Generated proxy source did not match the expected code.";
 }
 
