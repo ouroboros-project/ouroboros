d053c526c9a9d2f2d1ea7f893ab2bf702eed04af
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-07-10 15:38:35 -0300

    [opwig] Generated proxy's constructor.

--- src/opwig/gen/proxycodeprovider.h
+++ src/opwig/gen/proxycodeprovider.h
@@ -17,7 +17,9 @@ class ProxyCodeProvider {
             "#define OPWIG_GENERATED_"+proxied_class_name_+"_H_\n"
             "namespace generated {\n"
             "class "+proxied_class_name_+"_Proxy : public "+proxied_class_name_+" {\n"
-            "  public:\n";
+            "  public:\n"
+            "    "+proxied_class_name_+"_Proxy (const VirtualObj& the_proxy)\n"
+            "        : "+proxied_class_name_+"(), proxy_(the_proxy) {}\n";
     }
     std::string CloseProxy () const {
         return
