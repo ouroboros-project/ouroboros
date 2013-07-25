e98e488d957d68c2b6f204ff0491c7f3f25078e0
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-07-24 21:59:23 -0300

    [opwig-lua] Generating load function for wrapped module.

--- src/opwig/gen/lua/wrapperspecification.cxx
+++ src/opwig/gen/lua/wrapperspecification.cxx
@@ -13,11 +13,25 @@ Ptr<ConverterProvider> WrapperSpecification::GetConverterProvider () const {
 }
 
 string WrapperSpecification::FileHeader () const {
-    return "";
+    return
+        "#include <lua5.1/lauxlib.h>\n";
 }
 
 string WrapperSpecification::FinishFile () const {
-    return "";
+    return
+        "namespace {\n"
+        "luaL_Reg wrapped_functions[] = {\n"
+        "    { NULL, NULL }\n"
+        "};\n"
+        "} // unnamed namespace\n"
+        "/// [-1,+1,e]\n"
+        "int luaopen_"+module_name_+" (lua_State* L) {\n"
+        "    const char *mod_name = luaL_checkstring(L, 1);\n"
+        "    lua_settop(L, 0);\n"
+        "    luaL_register(L, mod_name, wrapped_functions);\n"
+        "    lua_setfield(L, LUA_GLOBALSINDEX, \""+module_name_+"\");\n"
+        "    return 1;\n"
+        "}\n";
 }
 
 string WrapperSpecification::WrapFunction(const md::Ptr<const md::Function>& obj) {
