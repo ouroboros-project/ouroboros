fdd0dc53569d466ad326a3f819b21249d71e02c3
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-07-25 14:27:35 -0300

    [opwig-lua] Dummy code for function wrapping generated.

--- test/acceptance/milestone-00/src/prompt.cxx
+++ test/acceptance/milestone-00/src/prompt.cxx
@@ -3,14 +3,14 @@
 
 #include <iostream>
 
-namespace prompt {
+//namespace prompt {
 
 using std::cout;
 using std::endl;
 
 void print (char* str) {
   cout << str << endl;
-}
+//}
 
 } // namespace prompt
 
