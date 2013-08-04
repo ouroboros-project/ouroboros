75539cdf6c79f2edaaee9a747412e8df6bc057c1
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-08-04 16:25:20 -0300

    [opwig-lua] Finshed lua talker.

--- test/acceptance/milestone-01/scripts/luatalker.lua
+++ test/acceptance/milestone-01/scripts/luatalker.lua
@@ -12,6 +12,18 @@ function main (name)
     if msg == "bye" then return true end
     if msg == "dude" then
       out.send_multimessage(name..": Say what?", 5)
+    elseif msg == "sqrt" then
+      out.send_message(name..": Of...?")
+      local arg = input.receive_number()
+      out.send_message(name..": 'Tis "..math.sqrt(arg))
+    elseif msg == "My favorite color is blue." then
+      out.send_message(name..": You sure?")
+      local answer = input.receive_confirmation()
+      if answer then
+        out.send_message(name..": Lame.")
+      else
+        out.send_message(name..": Indecisive fella.")
+      end
     end
   end
 end
