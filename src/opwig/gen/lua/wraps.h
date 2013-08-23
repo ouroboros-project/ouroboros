85163a154b7e3502726d53d55f17e6d167f18565
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-08-22 23:12:09 -0300

    [opwig-lua] Increased ModuleWrap constructor safety.
    -It is now uncopyable.
    -Only WrapperState may instantiate new ModuleWrap's

--- src/opwig/gen/lua/wraps.h
+++ src/opwig/gen/lua/wraps.h
@@ -3,6 +3,7 @@
 #define OPWIG_GEN_LUA_WRAPS_H_
 
 #include <opwig/md/ptr.h>
+#include <opa/utils/uncopyable.h>
 
 #include <string>
 #include <list>
@@ -16,7 +17,7 @@ struct DataWrap {
     std::string nesting;
 };
 
-struct ModuleWrap {
+struct ModuleWrap : public opa::utils::Uncopyable {
     std::string                     name, path;
     std::list<DataWrap>             functions;
     std::list<DataWrap>             getters;
@@ -24,6 +25,9 @@ struct ModuleWrap {
     std::list<md::Ptr<ModuleWrap>>  children;
     md::WeakPtr<ModuleWrap>         parent;
     bool                            open;
+  private:
+    ModuleWrap () {}
+    friend class WrapperState;
 };
 
 } // namespace lua
