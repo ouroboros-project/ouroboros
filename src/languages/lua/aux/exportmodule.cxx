2d7ce1a23a8bf291733b1dbc8a13cc1a873f55da
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-08-25 16:10:22 -0300

    [opwig/opa-lua] Moved some module exportation code to OPA.

---
+++ src/languages/lua/aux/exportmodule.cxx
@@ -0,0 +1,44 @@
+
+#include <languages/lua/aux/exportmodule.h>
+
+namespace opa {
+namespace lua {
+namespace aux {
+
+using std::string;
+
+namespace {
+
+/// This function uses the table on top of the stack to check for the parent module.
+/** If it is not there, the function creates it. Recursive. 
+ */
+void MakeParentModule (State& L, const ModuleInfo* parent) {
+    if (!parent) return;
+    MakeParentModule(L, parent->parent());
+    L.getfield(-1, parent->name());
+    if (L.isnil(-1)) {
+        L.pop(1);
+        L.newtable();
+        L.pushvalue(-1);
+        L.setfield(-3, parent->name());
+    }
+    L.remove(-2);
+}
+
+} // unnamed namespace
+
+void ExportModule (State& L, const ModuleInfo* info) {
+    if (L.gettop() > 1) {
+        L.remove(1);
+        L.settop(1);
+    } else {
+        L.settop(0);
+        L.pushvalue(Constant::GLOBALSINDEX());
+        MakeParentModule(L, info->parent());
+    }
+}
+
+} // namespace aux
+} // namespace lua
+} // namespace opa
+
---
+++ src/languages/lua/aux/exportmodule.h
@@ -0,0 +1,77 @@
+
+#ifndef OPA_LUA_AUX_EXPORTMODULE_H_
+#define OPA_LUA_AUX_EXPORTMODULE_H_
+
+#include <languages/lua/state.h>
+
+#include <string>
+#include <list>
+
+namespace opa {
+namespace lua {
+namespace aux {
+
+/// Represents information about a Lua module to be initialized.
+/** Mainly used by opa::lua::aux::ExportModule.
+ ** @see opa::lua::aux::ExportModule
+ */
+class ModuleInfo {
+
+  public:
+
+    /// Constructor.
+    /** The luaL_Reg[] parameters are arrays of lua_CFunction's, used by luaL_register().
+     ** @param the_name       Module's name (not the absolute path)
+     ** @param the_getters    The getters to the module's fields.
+     ** @param the_setters    The setters to the module's fields.
+     ** @param the_functions  The module's functions.
+     ** @param the_children   The module's child modules.
+     */
+    ModuleInfo (const std::string& the_name, luaL_Reg the_getters[], luaL_Reg the_setters[],
+                luaL_Reg the_functions[], const std::list<ModuleInfo>& the_children);
+
+    /// Gives the module's name.
+    /** @return std::string The module's name.
+     */
+    std::string name () const;
+
+    /// Gives the information about the module's supermodule.
+    /** @return const ModuleInfo* The module's parent information.
+     */
+    const ModuleInfo* parent () const;
+
+  private:
+
+    std::string           name_;
+    luaL_Reg              *getters_;
+    luaL_Reg              *setters_;
+    luaL_Reg              *funcions_;
+    ModuleInfo            *parent_;
+    std::list<ModuleInfo> children_;
+
+};
+
+inline ModuleInfo::ModuleInfo (const std::string& name, luaL_Reg the_getters[], luaL_Reg the_setters[],
+                        luaL_Reg the_functions[], const std::list<ModuleInfo>& the_children)
+    : getters_(the_getters), setters_(the_setters), funcions_(the_functions), parent_(nullptr),
+      children_(the_children) {
+    for (auto& child : children_)
+        child.parent_ = this;
+}
+
+inline std::string ModuleInfo::name () const {
+    return name_;
+}
+
+inline const ModuleInfo* ModuleInfo::parent () const {
+    return parent_;
+}
+
+void ExportModule (State& L, const ModuleInfo* info);
+
+} // namespace aux
+} // namespace lua
+} // namespace opa
+
+#endif
+
