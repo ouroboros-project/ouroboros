fbf90da652dd939c6560842538782c6e2ba72c2a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-10-03 13:45:42 -0300

    [cmake] Testing opwig-lua with specifiable output dir

--- src/opwig_lua.cxx
+++ src/opwig_lua.cxx
@@ -13,15 +13,19 @@ using std::string;
 using opwig::md::Ptr;
 
 int main (int argc, char** argv) {
-    list<string> inputs;
-    string       module_name = "Module";
+    list<string>  inputs;
+    string        module_name = "Module";
+    string        output_dir  = ".";
     for (++argv, --argc; argc; ++argv, --argc) {
-        size_t len = sizeof("--module-name=")-1;
+        size_t namearg_len = sizeof("--module-name=")-1,
+               dirarg_len  = sizeof("--output-dir=")-1;
         string arg = *argv;
-        if (arg.substr(0, len) == "--module-name=")
-            module_name = arg.substr(len);
+        if (arg.substr(0, namearg_len) == "--module-name=")
+            module_name = arg.substr(namearg_len);
+        else if (arg.substr(0, dirarg_len) == "--output-dir=")
+            output_dir = arg.substr(dirarg_len);
         else
-            inputs.push_back(*argv);
+            inputs.push_back(output_dir+*argv);
     }
     opa::lua::WrapperSpecification* lua_spec = new opa::lua::WrapperSpecification(module_name);
     return opwig::gen::Execute(module_name, inputs, Ptr<opwig::gen::WrapperSpecification>(lua_spec));
