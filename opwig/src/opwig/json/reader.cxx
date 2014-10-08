2080a9b1fe15517921c32d6562654d15e376a237
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2014-10-08 01:13:56 -0300

    Implement parsing of functions.
    Conflicts:
    	opwig/src/opwig/json/reader.cxx

--- opwig/src/opwig/json/reader.cxx
+++ opwig/src/opwig/json/reader.cxx
@@ -25,41 +25,61 @@ using opwig::md::Class;
 namespace opwig {
 namespace json {
 
-    namespace {
+namespace {
 
-        std::map<std::string, md::AccessSpecifier> access_specifier_mapper = {
-            { "private", md::AccessSpecifier::PRIVATE },
-            { "protected", md::AccessSpecifier::PROTECTED },
-            { "public", md::AccessSpecifier::PUBLIC },
-        };
+    auto void_type = md::Type::Create("void", false);
 
-        std::pair<Ptr<Scope>, std::string> GetScopeAndName(Ptr<Scope> initial_scope, const std::string& full_name) {
-            auto result = initial_scope;
-            std::size_t pos = 0, prev_pos = 0;
-            while ((pos = full_name.find("::", prev_pos)) != std::string::npos) {
-                auto name = full_name.substr(prev_pos, pos - prev_pos);
-                Ptr<Scope> next = result->NestedClass(name);
-                result = next ? next : result->NestedNamespace(name);
-                if (!result) {
-                    throw MissingScope(name, full_name);
-                }
-                prev_pos = pos + 2;
+    std::map<std::string, md::AccessSpecifier> access_specifier_mapper = {
+        { "private", md::AccessSpecifier::PRIVATE },
+        { "protected", md::AccessSpecifier::PROTECTED },
+        { "public", md::AccessSpecifier::PUBLIC },
+    };
+
+    std::pair<Ptr<Scope>, std::string> GetScopeAndName(Ptr<Scope> initial_scope, const std::string& full_name) {
+        auto result = initial_scope;
+        std::size_t pos = 0, prev_pos = 0;
+        while ((pos = full_name.find("::", prev_pos)) != std::string::npos) {
+            auto name = full_name.substr(prev_pos, pos - prev_pos);
+            Ptr<Scope> next = result->NestedClass(name);
+            result = next ? next : result->NestedNamespace(name);
+            if (!result) {
+                throw MissingScope(name, full_name);
             }
-            return std::make_pair(result, full_name.substr(prev_pos));
+            prev_pos = pos + 2;
         }
-
-        Ptr<Class> FindClass(Ptr<Scope> initial_scope, const std::string& full_name) {
-            Ptr<Scope> scope;
-            std::string name;
-            std::tie(scope, name) = GetScopeAndName(initial_scope, full_name);
-            return scope->NestedClass(name);
-        }
-
-        std::string FixTypename(const std::string& json_type) {
-            // FIXME: remove 'class', 'struct' words
-            return json_type;
-        }
+        return std::make_pair(result, full_name.substr(prev_pos));
     }
+
+    Ptr<Class> FindClass(Ptr<Scope> initial_scope, const std::string& full_name) {
+        Ptr<Scope> scope;
+        std::string name;
+        std::tie(scope, name) = GetScopeAndName(initial_scope, full_name);
+        return scope->NestedClass(name);
+    }
+
+    std::string FixTypename(const std::string& json_type) {
+        // FIXME: remove 'class', 'struct' words
+        return json_type;
+    }
+
+    Ptr<md::Function> CreateFunction(const JSONNode& data, bool is_method) {
+        std::vector<md::Parameter> parameter_list;
+        for (const auto& param : data["params"]) {
+            // FIXME: parameter_list.emplace_back(FixTypename(param.as_string()));
+            parameter_list.emplace_back(void_type);
+        }
+
+        auto func = md::Function::Create(data["name"].as_string(),
+                                         void_type, // FIXME: method["return"].as_string(),
+                                         parameter_list,
+                                         is_method ? data["pure"].as_bool() : false,
+                                         is_method ? data["virtual"].as_bool() : false);
+        func->set_deleted(data["deleted"].as_bool());
+        func->set_access(access_specifier_mapper.at(data["access"].as_string()));
+        // FIXME: where is const?
+        return func;
+    }
+}
 
 Reader::Reader(std::istream& in, md::Ptr<md::Namespace> global)
     : global_(global)
@@ -77,8 +97,6 @@ Reader::Reader(const std::string& s, md::Ptr<md::Namespace> global)
 bool Reader::parse() {
 
     auto json_root = libjson::parse(contents_);
-
-    auto void_type = md::Type::Create("void", false);
 
     if (json_root.find("namespaces") != json_root.end())
     for (const auto& ns : json_root["namespaces"]) {
@@ -105,25 +123,19 @@ bool Reader::parse() {
 
         if (c.find("methods") != c.end())
         for (const auto& method : c["methods"]) {
-
-            std::vector<md::Parameter> parameter_list;
-            for (const auto& param : method["params"]) {
-                // FIXME: parameter_list.emplace_back(FixTypename(param.as_string()));
-                parameter_list.emplace_back(void_type);
-            }
-
-            auto func = md::Function::Create(method["name"].as_string(),
-                                             void_type, // FIXME: method["return"].as_string(),
-                                             parameter_list,
-                                             method["pure"].as_bool(),
-                                             method["virtual"].as_bool());
-            func->set_deleted(method["deleted"].as_bool());
-            // FIXME: where is const?
-            func->set_access(access_specifier_mapper.at(method["access"].as_string()));
+            auto func = CreateFunction(method, true);
             tc->AddNestedFunction(func);
         }
 
         scope->AddNestedClass(tc);
+    }
+
+    if (json_root.find("functions") != json_root.end())
+    for (const auto& f : json_root["functions"]) {
+        Ptr<Scope> scope;
+        std::string name;
+        std::tie(scope, name) = GetScopeAndName(global_, f["qualified_name"].as_string());
+        scope->AddNestedFunction(CreateFunction(f, false));
     }
 
     return true;
